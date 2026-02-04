8af71ea7ac7ae2524a16d8bc4e528a04fe40c3e4
HeavyGuitarRiffs <truelee214@gmail.com>
2026-02-04 19:34:01 +0700

    fix: add valid help page component

---
+++ app/dashboard/help/page.tsx
@@ -0,0 +1,10 @@
+export default function HelpPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-semibold mb-2">Help Center</h1>
+      <p className="text-gray-600">
+        This page is currently under construction. More resources will be added soon.
+      </p>
+    </div>
+  );
+}
